4a507740b7ea34133a9d19a9f2bab380411bfe27
Rovin-karki <142002565+Rovin-karki@users.noreply.github.com>
2024-01-08 17:30:43 +0545

    Update build.py

--- pywebview_recipe/build.py
+++ pywebview_recipe/build.py
@@ -2,7 +2,7 @@ from pythonforandroid.recipe import PythonRecipe
 
 class PyWebViewRecipe(PythonRecipe):
     version = '4.4.1'
-    url = f'https://github.com/r0x0r/pywebview/archive/{version}.tar.gz'
+    url = f'https://github.com/r0x0r/pywebview/archive/refs/tags/{version}.tar.gz'
 
     depends = ['python3']
 
