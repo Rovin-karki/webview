354b51f763972765872b22acab3b3c185ab75d52
Rovin-karki <142002565+Rovin-karki@users.noreply.github.com>
2024-01-09 16:44:54 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import webview
+from android.permissions import Permission, request_permissions
+
+request_permissions([Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE])
+
 webview.create_window('Hello world', 'https://pywebview.flowrl.com/')
 webview.start()
