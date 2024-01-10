364e9c30eec8020ec386383949757ecdea0f1efa
Rovin-karki <142002565+Rovin-karki@users.noreply.github.com>
2024-01-10 12:55:31 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
-from flask import Flask, redirect
-from android.permissions import Permission, request_permissions
-
-request_permissions([Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE])
-
-app = Flask(__name__)
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
 
 
-@app.route('/')
-def index():
-    return redirect('https://www.pinterest.com')
+class SimpleKivyApp(App):
+    def build(self):
+        # Creating a BoxLayout as the root widget
+        layout = BoxLayout(orientation='vertical', spacing=10, padding=10)
+
+        # Creating a label
+        self.label = Label(text="Hello, Kivy!")
+
+        # Creating a button
+        button = Button(text="Click me!", on_press=self.on_button_click)
+
+        # Adding widgets to the layout
+        layout.add_widget(self.label)
+        layout.add_widget(button)
+
+        return layout
+
+    def on_button_click(self, instance):
+        # Change the label text when the button is clicked
+        self.label.text = "Button clicked!"
 
 
-if __name__ == "__main__":
-    app.run(debug=False, port=8080)
+if __name__ == '__main__':
+    SimpleKivyApp().run()
