81b9a5bb7840963a68e858654e7a6020bf6473a3
Rovin-karki <142002565+Rovin-karki@users.noreply.github.com>
2024-01-10 16:41:57 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-import webview
+from kivy.uix.widget import Widget
+from kivymd.app import MDApp
+from webview import WebView
+from kivy.lang.builder import Builder
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.screen import MDScreen
 
-class BoxLayoutApp(App):
+Builder.load_string("""
+<MyWebView>
+    MDFlatButton:
+        text: "Push"
+        pos_hint: {"center_x": .5, "center_y": .4}
+        on_press: root.Push()
+""")
+
+class MyWebView(MDScreen):
+    def Push(self):
+        WebView("https://www.google.com")
+
+
+class MyWebApp(MDApp):
     def build(self):
-        # Create a BoxLayout with vertical orientation
-        box_layout = BoxLayout(orientation='vertical', spacing=10, padding=10)
+        return MyWebView()
 
-        # Create a button to open the webview window
-        webview_button = Button(text='Open Webview')
-        webview_button.bind(on_press=self.open_webview)
-
-        # Add the button to the BoxLayout
-        box_layout.add_widget(webview_button)
-
-        return box_layout
-
-    def open_webview(self, instance):
-        # Code to create and start the webview window
-        webview.create_window('Hello world', 'https://pywebview.flowrl.com/')
-        webview.start()
 
 if __name__ == '__main__':
-    BoxLayoutApp().run()
+    MyWebApp().run()
