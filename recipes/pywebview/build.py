f583f8cf79a952bcd25c85a093671e0a40bb5f3c
Rovin-karki <142002565+Rovin-karki@users.noreply.github.com>
2024-01-10 08:05:50 +0545

    Delete recipes/pywebview directory

--- recipes/pywebview/build.py
+++
@@ -1,9 +0,0 @@
-from pythonforandroid.recipe import PythonRecipe
-
-class PyWebViewRecipe(PythonRecipe):
-    version = '4.4.1'
-    url = f'https://github.com/r0x0r/pywebview/archive/{version}.tar.gz'
-
-    depends = ['python3']
-
-recipe = PyWebViewRecipe()
