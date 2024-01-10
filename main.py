30eb46344b4c899e3d5fe266aa47e211d19e698f
Rovin-karki <142002565+Rovin-karki@users.noreply.github.com>
2024-01-10 08:06:29 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-import webview
+from flask import Flask, redirect
 from android.permissions import Permission, request_permissions
 
 request_permissions([Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE])
 
-webview.create_window('Hello world', 'https://pywebview.flowrl.com/')
-webview.start()
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return redirect('https://www.pinterest.com')
+
+
+if __name__ == "__main__":
+    app.run(debug=False, port=8080)
